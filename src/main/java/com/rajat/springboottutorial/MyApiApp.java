7d7ac4c089e48330aa37b25549dc4bdfe79cd748
Rajat Parida <rajatparida86@gmail.com>
2018-07-20 22:16:16 +0200

    Create springboot app

---
+++ src/main/java/com/rajat/springboottutorial/MyApiApp.java
@@ -0,0 +1,11 @@
+package com.rajat.springboottutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApiApp.class, args);
+    }
+}
